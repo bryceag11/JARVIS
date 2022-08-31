c807dcbcb885a2ab919ddf027132850b43f6d455
Bradley Hargis <bradley.hargis@ymail.com>
2022-08-31 03:10:04 +0100

    Set command to run in background, added option to set camera resolution

--- start_cam.sh
+++ start_cam.sh
@@ -2,7 +2,8 @@
 
 FILE="$HOME/.ssh/selfsign.key"
 DEVICE_ID="046d:0825"
-
+WIDTH="1280"
+HEIGHT="720"
 
 #gen keys if they do not exist
 if [ ! -d "$HOME/.ssh" ]; then
@@ -18,5 +19,11 @@ fi
 #stop current uv4l services
 sudo service uv4l* stop
 
-#run streaming server
-exec sudo uv4l --syslog-host localhost --driver uvc --device-id $DEVICE_ID -n video0 /dev/uv4l --verbosity 8 -f --server-option --use-ssl=yes --server-option --ssl-private-key-file=$HOME/.ssh/selfsign.key --server-option --ssl-certificate-file=$HOME/.ssh/selfsign.crt --server-option --enable-webrtc=yes --server-option --enable-webrtc-video=yes
+#run streaming server in background
+exec sudo uv4l --syslog-host localhost --driver uvc --device-id $DEVICE_ID -n video0 /dev/uv4l --verbosity 8 -f --server-option --use-ssl=yes --server-option --ssl-private-key-file=$HOME/.ssh/selfsign.key --server-option --ssl-certificate-file=$HOME/.ssh/selfsign.crt --server-option --enable-webrtc=yes --server-option --enable-webrtc-video=yes &
+
+sleep 15
+
+#Set camera resolution and format (not included here)
+v4l2-ctl --set-fmt-video=width=$WIDTH,height=$HEIGHT -d /dev/video0
+#v412-ctl --set-fmt-video=width=$WIDTH,height=$HEIGHT,pixelformat="H264" -d /dev/video0
