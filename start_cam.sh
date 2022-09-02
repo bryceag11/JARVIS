21778baf1f3e9832b05afd1a90d2d7e652891e07
BradleyHargis <77749668+BradleyHargis@users.noreply.github.com>
2022-09-01 22:34:02 -0400

    Got webrtc streaming working at low resolution

--- start_cam.sh
+++ start_cam.sh
@@ -2,8 +2,9 @@
 
 FILE="$HOME/.ssh/selfsign.key"
 DEVICE_ID="046d:0825"
-WIDTH="1280"
-HEIGHT="720"
+DEVICE_NAME="video0"
+WIDTH="1920"
+HEIGHT="1080"
 
 #gen keys if they do not exist
 if [ ! -d "$HOME/.ssh" ]; then
@@ -19,13 +20,25 @@ fi
 #stop current uv4l services
 #sudo service uv4l* stop
 
+#set camera resolution
+v4l2-ctl --set-fmt-video=width=$WIDTH,height=$HEIGHT -d /dev/video0
+
+#set camera format
+#TODO not working
+#v412-ctl --set-fmt-video=width=$WIDTH,height=$HEIGHT,pixelformat="MPJEG" -d /dev/video0
+
+
 #run streaming server in background
-#exec sudo uv4l --syslog-host localhost --driver uvc --device-id $DEVICE_ID -n video0 /dev/uv4l --verbosity 8 -f --server-option --use-ssl=yes --server-option --ssl-private-key-file=$HOME/.ssh/selfsign.key --server-option --ssl-certificate-file=$HOME/.ssh/selfsign.crt --server-option --enable-webrtc=yes --server-option --enable-webrtc-video=yes
+OPTIONS="\
+--syslog-host localhost \
+--external-driver \
+--device-name $DEVICE_NAME \
+--device-id $DEVICE_ID \
+--driver-name $DEVICE_NAME \
+--verbosity 8 \
+--server-option --use-ssl=yes \
+--server-option --ssl-private-key-file=$HOME/.ssh/selfsign.key \
+--server-option --ssl-certificate-file=$HOME/.ssh/selfsign.crt \
+"
 
-exec sudo uv4l --syslog-host localhost -external-driver --device-id $DEVICE_ID --verbosity 8 -f --server-option --use-ssl=yes --server-option --ssl-private-key-file=$HOME/.ssh/selfsign.key --server-option --ssl-certificate-file=$HOME/.ssh/selfsign.crt
-
-#sleep 15
-
-#Set camera resolution and format (not included here)
-#v4l2-ctl --set-fmt-video=width=$WIDTH,height=$HEIGHT -d /dev/video0
-#v412-ctl --set-fmt-video=width=$WIDTH,height=$HEIGHT,pixelformat="H264" -d /dev/video0
+uv4l $OPTIONS
