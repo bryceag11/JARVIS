39bf3a48d2f20e3193d3962c04072bb616e7ceff
BradleyHargis <77749668+BradleyHargis@users.noreply.github.com>
2022-09-03 21:34:57 -0400

    Update install.sh
    Added zoom install commands

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo "Starting install..."
 
+cd ~/
+
 #update pi drivers
 apt-get update
 apt-get full-upgrade
@@ -9,19 +11,22 @@ apt-get full-upgrade
 #install vim
 apt-get install vim
 
-#Add uv4l drivers to source list
-curl https://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
-echo "deb https://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" | sudo tee /etc/apt/sources.list.d/uv4l.list
+#install zoom
+sudo apt-get install libxcb-xtest0
+wget https://zoom.us/client/5.11.10.4400/zoom_x86_64.tar.xz
+tar xvf zoom_x86_64.tar.xz
+rm zoom_x86_64.tar.xz
 
-#install uv4l drivers
-sudo apt-get update
-sudo apt-get install uv4l uv4l-server
-
-#cleanup
-apt-get autoremove
-
-#update pi firmware
-rpi-update
+#install box86
+apt install git build-essential cmake
+git clone https://github.com/ptitSeb/box86
+cd ~/box86
+mkdir build
+cd build
+cmake .. -DRPI4=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+make -j$(nproc)
+make install
+systemctl restart systemd-binfmt
 
 echo "Install finished"
 echo "Raspberry Pi will now reboot"
