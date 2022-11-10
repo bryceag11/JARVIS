6a94b9175eda4169545d7c3c875a899933e264aa
Bradley Hargis <bradley.hargis@ymail.com>
2022-11-10 15:33:23 -0500

    Auto fetch IP

--- WebSocketServer.py
+++ WebSocketServer.py
@@ -6,6 +6,10 @@ import socket
 IPAddr=socket.gethostbyname(socket.getfqdn())
 print(IPAddr)
 
+import netifaces as ni
+ip = ni.ifaddresses('wlo1')[ni.AF_INET][0]['addr']
+print("My IP is: " + ip)
+
 # create handler for each connection
 data = ""
 
@@ -16,7 +20,7 @@ async def handler(websocket, path):
         print(reply)
         await websocket.send(reply)
  
-start_server = websockets.serve(handler, IPAddr, 8000)
+start_server = websockets.serve(handler, ip, 8000)
  
 asyncio.get_event_loop().run_until_complete(start_server)
  
