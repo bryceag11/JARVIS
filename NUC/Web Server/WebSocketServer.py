57d983134f1d3d16480f1416956a86feb9a8914b
Caleb Lytle <caleblytle@uky.edu>
2022-11-12 11:56:43 -0500

    added port to rpi ws ip

--- NUC/Web Server/WebSocketServer.py
+++ NUC/Web Server/WebSocketServer.py
@@ -7,7 +7,7 @@ print("NUC IP is: " + nucIP)
 
 try:
     rpiWS = websockets.WebSocket()
-    rpiWS.connect("ws://10.163.149.122")
+    rpiWS.connect("ws://10.163.149.122:8080")
     rpiWS.send(nucIP)
 except:
     print("connection to raspberry pi failed")
