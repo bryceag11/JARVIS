5262e2d4cdc6ce40cf163378682bc11ef73bd2be
Caleb Lytle <caleblytle@uky.edu>
2022-12-02 12:14:19 -0500

    changed port

--- NUC/Web Server/wsClient.py
+++ NUC/Web Server/wsClient.py
@@ -3,7 +3,7 @@ import asyncio
 
 data = ""
 async def connectRpi():
-    uri = "ws://10.163.149.122:8000"
+    uri = "ws://10.163.149.122:8001"
     async with websockets.connect(uri) as websocket:
         await websocket.recv(data)
         print("data received {data}")
