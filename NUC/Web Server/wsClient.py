b182b12467485f1cdc307f754b730a5310eab9fa
Bryce <67086260+bryceag11@users.noreply.github.com>
2023-09-16 01:35:06 -0400

    Delete NUC/Web Server directory

--- NUC/Web Server/wsClient.py
+++
@@ -1,9 +0,0 @@
-import websockets
-import asyncio
-
-data = ""
-async def connectRpi():
-    uri = "ws://10.163.149.122:8001"
-    async with websockets.connect(uri) as websocket:
-        await websocket.recv(data)
-        print("data received {data}")
