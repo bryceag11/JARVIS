02a6ffc961e32a9632ce40377502dbadf1de61a2
Bryce <67086260+bryceag11@users.noreply.github.com>
2023-09-16 01:40:29 -0400

    Create main.py
    Main file for GUI app control

---
+++ GUI_APP/main.py
@@ -0,0 +1,12 @@
+"""
+File: main.py
+Author: Bryce Grant
+Class: EE491 (ECE Capstone II)
+Description: Integration of wrapped GUI with the zoom SDK
+Provide live feed from the camera on the robot to allow user to see robot's environment in real time
+"""
+
+import gui
+
+if __name__ == "__main__":
+    gui.start()
