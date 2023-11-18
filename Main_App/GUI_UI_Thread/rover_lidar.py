77186d38389eecee067ee9f4253cb0c69cbc8773
Bryce <67086260+bryceag11@users.noreply.github.com>
2023-11-18 16:20:05 -0500

    Update rover_lidar.py
    Updated autonomous nav

--- Main_App/GUI_UI_Thread/rover_lidar.py
+++ Main_App/GUI_UI_Thread/rover_lidar.py
@@ -28,20 +28,10 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
-# setting visualization parameters
-pos=0
-vers=1
-v3dr=180
-pos_init= 0
-pos_end = 32
-pos_step=2
-lidar_chunk_size=1280*2
-dist_limit=2000
 
 # setting control parameters
 speed = 25
 direction = 0.0 # direction in degrees
-increment_turn = 0.4
 
 # initializing motor commands
 motor = MotorCommands()
@@ -61,126 +51,122 @@ Obj = PyLidar3.YdLidarX4(port) #PyLidar3.YDLidarX4(port,chunk_size)
 motor_thread_run = True
 object_detected = False
 
+
+# Constants for decision-making
+STOP_DISTANCE = 900  # Distance threshold to stop the robot in front of an object
+BACKWARD_TIME = 0.2  # Time duration to move backward
+RIGHT_TURN_DISTANCE = 1000  # Distance threshold to trigger a right turn
+
 '''
 Drive function for motor thread
 '''
-def drive():
+def drive(center_average, left_lidar_data, right_lidar_data):
     global motor_thread_run, object_detected
 
-    while motor_thread_run:
-        data = next(gen) # get lidar scan data
-        left_total = 0
-        right_total = 0
-        center_total = 0
-        bias = 0
-        
-        time.sleep(0.2)  # Delay for processing stability
-        # measurement and calculation of averages and values
-        for angle in range(340,360):  # measure the center
-            if(data[angle]> 0):
-                center_total = center_total + data[angle]
-        for angle in range(0,20):  # measure the center
-            if(data[angle]> 0):
-                center_total = center_total + data[angle]
-        center_average = round(center_total/40,2)
-        for angle in range(20,100):  # measure the right side
-            if(data[angle]> 0):
-                right_total = right_total + data[angle]
-        right_average = round(right_total/80,2)
-        for angle in range(260,340): # measure the left side
-            if(data[angle]> 0):
-                left_total = left_total + data[angle]
-        left_average = round(left_total/80,2)
-        print("Center average", center_average)
-        print("Left average", left_average)
-        print("Right average", right_average)
-        
-        # convert to Cartesian coordinates
-        left_y = math.sin(math.pi/4) * left_average
-        left_x = -1*math.cos(math.pi/4) * left_average
-        right_y = math.sin(math.pi/4) * right_average
-        right_x = math.cos(math.pi/4) * right_average
-        center_y = center_average
+    # Check if an object is detected in front
+    if center_average < STOP_DISTANCE:
+        # Set flag for object detected
+        object_detected = True
 
-        sum_x = round(left_x + right_x,2) # calculate sum of x-coordinates
-        sum_y = round(center_y - (left_y + right_y)/2,2) # calculate sum of y-coordinates
-        
-        if sum_y < 100:
-            sum_y = 100  # ensure minimum value for y
-        
-        print("Sum x:", sum_x, "Sum y: ", sum_y)
+        # Stop the rover
+        motor.stop_motors(ser)
 
-        sum_angle = math.atan2(sum_x,sum_y) # calculate steering angle in radians
+        # Check if there are objects all around (left, right, center)
+        if all(data < STOP_DISTANCE for data in left_lidar_data) and all(data < STOP_DISTANCE for data in right_lidar_data):
+            # Move backward for a specified duration
+            motor.go_backward(ser, speed)
+            time.sleep(BACKWARD_TIME)
+            object_detected = False  # Reset object detection flag after moving backward
 
-        print ("Sum Steer", round(sum_angle,2))
-
-                # Assuming left_lidar_data and right_lidar_data contain data from left and right lidar respectively
-        left_lidar_data = []  # Replace with actual left lidar data
-        right_lidar_data = []  # Replace with actual right lidar data
-
-        # Check if an object is detected within 900mm in front
-        if center_average < 900:
-            # Set flag for object detected
-            object_detected = True
-
-            # Stop the rover
-            motor.stop_motors(ser)
-
-            # Determine whether to turn left or right based on left and right lidar data
-            if max(left_lidar_data) > max(right_lidar_data):
+            # Check if there's enough space on the right to turn
+            if all(data > RIGHT_TURN_DISTANCE for data in right_lidar_data):
                 # Turn right
                 motor.turn_right(ser, speed)
-            else:
-                # Turn left
-                motor.turn_left(ser, speed)
+                time.sleep(1)  # Adjust duration for the turn
+                motor.go_forward(ser, speed)  # Resume forward movement
+
+        # Resume normal movement if not all directions are blocked
         else:
-            # If an object was previously detected, continue moving backwards
-            if object_detected:
-                motor.go_backward(ser, speed)  # Move backward
-                time.sleep(1)  # Continue moving backward for 1 second
-                object_detected = False  # Reset object detection flag
+            motor.go_forward(ser, speed)
 
-            # Convert radians to degrees and ensure it's within [0, 360)
-            direction = sum_angle * (180 / math.pi) % 360
-
-            # Continue normal movement based on calculated direction
-            motor.go_dir(ser, speed, direction)
-        
-        time.sleep(0.2)  # Delay for processing stability
-
-        # # convert radians to degrees and ensure it's within [0, 2π]
-        # direction = sum_angle * (180 / math.pi) % 360
-        # sum_dist = math.sqrt(sum_x**2 + sum_y**2)
+    time.sleep(0.2)  # Delay for processing stability
 
 
 '''
 Main function for data processing and motor control
 '''
 def main():
-    global motor_thread_run, object_detected 
-    try:
-        if(Obj.Connect()): # establish connection to LiDAR device
-            print(Obj.GetDeviceInfo())
-            gen = Obj.StartScanning() 
-            threading.Thread(target=drive).start() # start motor control thread
-        else:
-            print("Error connecting to device")
+    global direction
+    global motor_thread_run, object_detected
 
-        while True:
-            data = next(gen)
+    while motor_thread_run:
+        start_time = time.time()
+        while time.time() - start_time < 0.2:
 
-    except (KeyboardInterrupt, SystemExit):
-        print("Quitting")
+            data = next(gen) # get lidar scan data
+            left_total = 0
+            right_total = 0
+            center_total = 0
+            bias = 0
+            
+            time.sleep(0.2)  # Delay for processing stability
+            # measurement and calculation of averages and values
+            for angle in range(340,360):  # measure the center
+                if(data[angle]> 0):
+                    center_total = center_total + data[angle]
+            for angle in range(0,20):  # measure the center
+                if(data[angle]> 0):
+                    center_total = center_total + data[angle]
+            center_average = round(center_total/40,2)
+            for angle in range(20,100):  # measure the right side
+                if(data[angle]> 0):
+                    right_total = right_total + data[angle]
+            right_average = round(right_total/80,2)
+            for angle in range(260,340): # measure the left side
+                if(data[angle]> 0):
+                    left_total = left_total + data[angle]
+            left_average = round(left_total/80,2)
+            print("Center average", center_average)
+            print("Left average", left_average)
+            print("Right average", right_average)
+            
+            # convert to Cartesian coordinates
+            left_y = math.sin(math.pi/4) * left_average
+            left_x = -1*math.cos(math.pi/4) * left_average
+            right_y = math.sin(math.pi/4) * right_average
+            right_x = math.cos(math.pi/4) * right_average
+            center_y = center_average
 
-        # set the motor thread flag false
-        motor_thread_run = False
+            sum_x = round(left_x + right_x,2) # calculate sum of x-coordinates
+            sum_y = round(center_y - (left_y + right_y)/2,2) # calculate sum of y-coordinates
+            
+            if sum_y < 100:
+                sum_y = 100  # ensure minimum value for y
+            
+            print("Sum x:", sum_x, "Sum y: ", sum_y)
 
-        # stop motors if not already stopped
-        motor.stop_motors(ser)
+            sum_angle = math.atan2(sum_x,sum_y) # calculate steering angle in radians
 
-        # disconnect the lidar
-        Obj.StopScanning() 
-        Obj.Disconnect()
+            print ("Sum Steer", round(sum_angle,2))
 
-if __name__ == "__main__":
+
+        drive(center_average, left_average, right_average)
+
+
+try:
+    if(Obj.Connect()):
+        print(Obj.GetDeviceInfo())
+        gen = Obj.StartScanning()
+        threading.Thread(target=drive).start()
+    else:
+        print("Error connecting to device")
     main()
+except (KeyboardInterrupt, SystemExit):
+    print("Quitting")
+
+    # stop motors if not already stopped
+    motor.stop_motors(ser)
+
+    # disconnect the lidar
+    Obj.StopScanning()
+    Obj.Disconnect()
